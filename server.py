a9ff9f7c865f748112009cf20c5e6084a5b241f0
Raymond Mason <dmonyar@wharton.upenn.edu>
2021-12-09 13:15:09 -0500

    Server Dashboard

--- server.py
+++ server.py
@@ -1,14 +1,20 @@
-import cv2, socket, numpy, pickle
+import cv2, socket, numpy, pickle, time
 s=socket.socket(socket.AF_INET , socket.SOCK_DGRAM)
 ip="localhost"
 port=307
 s.bind((ip,port))
+total=0
+start_time=time.time()
 while True:
     x=s.recvfrom(100000000)
+    end_time=time.time()
+    total+=len(x[0])
+    if (total % 500000):
+        print("Total Transfer (bytes) >=" + str(total))
+        print("Bytes / s : " + str(len(x[0])/(1000 * (end_time - start_time))))
     clientip = x[1][0]
     data=x[0]
     data=pickle.loads(data)
-    print(type(data))
     data = cv2.imdecode(data, cv2.IMREAD_COLOR)
     cv2.imshow('my pic', data) #to open image
     if cv2.waitKey(10) == 13:
